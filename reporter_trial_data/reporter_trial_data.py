69cea4b3e51611ca18cf931a7046ff837f197de6
Alexander Krigsman <alexander.krigsman@dongit.nl>
2022-01-13 17:10:32 +0100

    Read Reporter API token from environment

--- reporter_trial_data/reporter_trial_data.py
+++ reporter_trial_data/reporter_trial_data.py
@@ -1,10 +1,14 @@
 from getpass import getpass
+import os
 
 from reporter.client import Reporter
 
 
 def main():
-    api_token = getpass("Reporter API token: ")
+    if "REPORTER_API_TOKEN" in os.environ:
+        api_token = os.environ["REPORTER_API_TOKEN"]
+    else:
+        api_token = getpass("Reporter API token: ")
 
     reporter = Reporter(api_token)
     result = reporter.findings.list(filter={"severity": "11"})
