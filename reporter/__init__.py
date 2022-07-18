78604b174a614ee47facbaafef0189739171be90
Alexander Krigsman <alexander.krigsman@dongit.nl>
2022-07-18 10:45:24 +0200

    Export Reporter in base __init__.py

--- reporter/__init__.py
+++ reporter/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.1.0"
 
 from reporter.base import *
-from reporter.client import Reporter
+from reporter.client import *
 from reporter.exceptions import *
 from reporter.objects import *
 from reporter.object_includes import *
