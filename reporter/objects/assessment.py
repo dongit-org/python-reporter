6cee8ce0ba8ff06ce90b1928c78d6383072d27ea
Stefan van der Lugt <stefan.vanderlugt@dongit.nl>
2024-03-22 15:37:59 +0100

    Add methods to AssessmentManager for downloading reports

--- reporter/objects/assessment.py
+++ reporter/objects/assessment.py
@@ -1,4 +1,5 @@
 # pylint: disable = missing-module-docstring, missing-class-docstring
+from typing import Any
 
 from reporter.base import RestManager, RestObject
 from reporter.mixins import CreateMixin, GetMixin, ListMixin, UpdateMixin, DeleteMixin
@@ -31,6 +32,60 @@ class Assessment(RestObject):
 class AssessmentManager(RestManager, GetMixin, ListMixin, UpdateMixin, DeleteMixin):
     _path = "assessments"
     _obj_cls = Assessment
+
+    def get_full_report(self, id, **kwargs: Any) -> bytes:
+        """Get the full PDF report of an assessment.
+
+        Args:
+            id: The ID of the assessment
+            **kwargs: Extra options to pass to the underlying
+                :func:`reporter.Reporter.http_request` call.
+
+        Returns:
+            The full report as a bytestring.
+
+        Raises:
+            ReporterHttpError: If raised by the underlying call to
+                :func:`reporter.Reporter.http_request`.
+
+        """
+        path = f"{self._path}/{id}/pdf-reports/full"
+
+        result = self.reporter.http_request(
+            verb="get",
+            path=path,
+            headers={"Accept": "*/*"},
+            **kwargs,
+        )
+
+        return result.content
+
+    def get_management_report(self, id, **kwargs: Any) -> bytes:
+        """Get the management PDF report of an assessment.
+
+        Args:
+            id: The ID of the assessment
+            **kwargs: Extra options to pass to the underlying
+                :func:`reporter.Reporter.http_request` call.
+
+        Returns:
+            The management report as a bytestring.
+
+        Raises:
+            ReporterHttpError: If raised by the underlying call to
+                :func:`reporter.Reporter.http_request`.
+
+        """
+        path = f"{self._path}/{id}/pdf-reports/management"
+
+        result = self.reporter.http_request(
+            verb="get",
+            path=path,
+            headers={"Accept": "*/*"},
+            **kwargs,
+        )
+
+        return result.content
 
 
 class ClientAssessmentManager(RestManager, CreateMixin):
