9bdbce42523b85deb57a7534f5d1ab62c398db42
Stefan van der Lugt <stefan.vanderlugt@dongit.nl>
2025-09-26 15:19:38 +0200

    Update theme test

--- tests/functional/test_theme.py
+++ tests/functional/test_theme.py
@@ -1,10 +1,12 @@
-import pytest
-
-import reporter
-from reporter import Reporter
+from reporter import Reporter, ReportPage
 
 
 def test_theme_operations(rc: Reporter) -> None:
-    themes = rc.themes.list()
+    themes = rc.themes.list(include="pages")
 
-    assert len(themes) > 0 and themes[0].name == "Reporter"
+    assert len(themes) == 1
+    assert themes[0].name == "Reporter"
+    assert len(themes[0].pages) == 2
+    assert isinstance(themes[0].pages[0], ReportPage)
+    assert themes[0].pages[0].name == "Front Page"
+
