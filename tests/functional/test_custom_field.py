ffd894d3d0716e12ffd903fbb3b7c175aad68881
Stefan van der Lugt <stefan.vanderlugt@dongit.nl>
2025-09-26 16:57:11 +0200

    Add custom field test

---
+++ tests/functional/test_custom_field.py
@@ -0,0 +1,21 @@
+from reporter import Reporter
+from . import Artisan
+
+
+def test_custom_field_operations(rc: Reporter, artisan: Artisan) -> None:
+    # Create a custom field through Artisan, as there is currently no way to do so through the API
+    php_code = """
+    $field = \\App\\Models\\CustomField::factory()->create([
+        'name' => 'test_custom_field',
+        'description' => 'Test custom field',
+    ]);
+    echo $field->id;
+    """
+    output = artisan(execute=php_code)
+    custom_field_id = output.strip()
+
+    custom_fields = rc.custom_fields.list()
+
+    assert len(custom_fields) > 0
+    assert custom_fields[0].id == custom_field_id
+    assert custom_fields[0].name == "c_test_custom_field"
