44af90162ec3548e6710bd1770c8f55de4693f6a
Stefan van der Lugt <stefan.vanderlugt@dongit.nl>
2025-10-01 16:06:02 +0200

    Make type upload more flexible

--- reporter/__init__.py
+++ reporter/__init__.py
@@ -1,9 +1,10 @@
 """Wrapper around the Reporter API."""
 
+from __future__ import annotations
 __version__ = "0.1.0"
-
 from reporter.base import *
 from reporter.client import *
 from reporter.exceptions import *
 from reporter.objects import *
 from reporter.object_includes import *
+from reporter.types import *
